2b81849c2fdd25a6f1618ea39f5915a0596c1372
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-01-06 02:14:10 +0100

    added the class of user

--- classes/user.php
+++ classes/user.php
@@ -1,41 +1,60 @@
 <?php
 
-include "crud.php";
+require 'crud.php';
 
-class user extends CRUD {
+class User {
+    protected $id;  
     protected $name;
     protected $email;
+    protected $password;
+    protected $role = 'simpleuser';  // to maek it default when a user created an account
+    protected $profilePhoto;
     protected $gender;
+    protected $bio;
+    protected $birthdate;
+    protected $crud;
 
-    public function __construct(){
+    public function __construct($db, $data = []) {
+        $this->crud = new CRUD($db, 'users');
 
+        $this->name = $data['name'] ?? null;
+        $this->email = $data['email'] ?? null;
+        $this->password = $data['password'] ?? null;
+        $this->role = $data['role'] ?? $this->role;  // if admin created the user , he can assgin the role else, the role default will be assigned
+        $this->profilePhoto = $data['profilePhoto'] ?? null;
+        $this->gender = $data['gender'] ?? null;
+        $this->bio = $data['bio'] ?? null;
+        $this->birthdate = $data['birthdate'] ?? null;
     }
 
-    protected function update_profile($name,$email, $gender,$id){
-        //after the validation;
-        $this->table ="users";
-        $this-> update(["name"=>$name,"email"=>$email, "gender"=>$gender],"id=$id");
+    public function register() {
+        // Hash the password using Argon2
+        $this->password = password_hash($this->password, PASSWORD_ARGON2ID);
+        $data = [
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password,
+            'role' => $this->role,
+            'profilePhoto' => $this->profilePhoto,
+            'gender' => $this->gender,
+            'bio' => $this->bio,
+            'birthdate' => $this->birthdate
+        ];
+        return $this->crud->create($data);
     }
 
-
-    protected function view_articles(){
-        $this->table = "articles";
-        $this->read();
+    public function login() {
+        //hta nchof sessions o cookies
     }
 
-   
+    public function logout() {
+        // hta nchof sessions o cookies 
+    }
 
-
-  
-   }
-
-
-
- protected function  
-
-
-
-
+    public function manageProfile($data, $conditions) {
+        return $this->crud->update($data, $conditions);
+    }
+}
 
 
 
--- index.php
+++ index.php
@@ -14,7 +14,31 @@ if ($db) {
 }
 
 
+// Create instances
+$category = new Category($db);
+$tag = new Tag($db);
 
+// Test create operation
+$newCategory = ['name' => 'Web Development'];
+$category->create($newCategory);
+
+$newTag = ['name' => 'PHP'];
+$tag->create($newTag);
+
+// Test read operation
+$categories = $category->read();
+$tags = $tag->read();
+
+print_r($categories);
+print_r($tags);
+
+// Test update operation
+$category->update(['name' => 'Frontend Development'], ['id' => 1]);
+$tag->update(['name' => 'JavaScript'], ['id' => 1]);
+
+// Test delete operation
+$category->delete(['id' => 2]);
+$tag->delete(['id' => 2]);
 
 ?>
 
